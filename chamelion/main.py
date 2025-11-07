135c64df19c18fb6cf745d220a77b42150885911
Your Name <lgdelacruz92@gmail.com>
2025-11-07 06:21:38 -0500

    okay

---
+++ chamelion/main.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+
+# Get the parent directory and add it to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+
+from common import *
+
+def make_bw(img, pixels, npix):
+    for x in range(img.width):
+        for y in range(img.height):
+            r, g, b = pixels[x, y]
+            v = ( r + g + b ) // 3
+
+            if v < 35:
+                npix[x,y] = (200, 0, 0)
+            elif 35 < v and v < 130:
+                npix[x,y] = (0, 200, 0)
+
+            else:
+                npix[x,y] = (0, 0, 200)
+                
+            # npix[x,y] = (v, v, v)
+
+
+def make_dark(img, pixels, npix):
+    for x in range(img.width):
+        for y in range(img.height):
+            r, g, b = pixels[x, y]
+            v = ( r + g + b ) // 3
+            npix[x,y] = (v, v, v)
+
+if __name__ == '__main__':
+    pixels, img = load_image(inputfile)
+    npix, nimg = make_new_image(img)
+
+
+    make_bw(img, pixels, npix)
+
+    nimg.save(f'{outputfile}.jpg')
